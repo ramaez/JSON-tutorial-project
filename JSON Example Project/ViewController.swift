13b3731cbd1e69e8161662a6d0ae461256c7bd91
Ruben <ruben@odin-inc.com>
2018-10-23 14:10:50 -0400

    Initial Commit

---
+++ JSON Example Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON Example Project
+//
+//  Created by Ken Thomsen on 10/23/18.
+//  Copyright © 2018 Ruben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
