936087d2062de0a4c753d3fab0817e1fcf4f3ff5
Ruben <ruben@odin-inc.com>
2018-11-06 11:38:27 -0500

    Added GCD to viewDidLoad() so that JSON is downloaded on background thread but the error alert and reloadData() are performed on the main thread.

--- JSON Example Project/ViewController.swift
+++ JSON Example Project/ViewController.swift
@@ -23,22 +23,25 @@ class ViewController: UITableViewController {
         } else {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
-        
-        if let url = URL(string: urlString) {
-            if let data = try? String(contentsOf: url) {
-                let json = JSON(parseJSON: data)
-                
-                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
-                    parse(json: json)
+        DispatchQueue.global(qos: .userInitiated).async { [unowned self] in
+            
+            if let url = URL(string: urlString) {
+                if let data = try? String(contentsOf: url) {
+                    let json = JSON(parseJSON: data)
+                    
+                    if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                        self.parse(json: json)
+                    } else {
+                        self.showError()
+                    }
                 } else {
-                    showError()
+                    self.showError()
                 }
             } else {
-                showError()
+                self.showError()
             }
-        } else {
-            showError()
         }
+        //showError()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -67,15 +70,18 @@ class ViewController: UITableViewController {
             let obj = ["title":title, "body": body, "sigs": sigs]
             petitions.append(obj)
         }
-        
-        tableView.reloadData()
+        DispatchQueue.main.async { [unowned self] in
+            self.tableView.reloadData()
+        }
         
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
+        DispatchQueue.main.async { [unowned self] in
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
     }
 
 
